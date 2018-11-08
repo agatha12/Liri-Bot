24dc04248c064447db1564e9247a92b7d925d2e7
agatha12 <agkiecun@gmail.com>
2018-11-08 14:40:51 -0500

    everything but concert

--- liri.js
+++ liri.js
@@ -11,46 +11,54 @@ function song(input) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log("Artist: " + data.tracks.items[0].artists[0].name);
-        console.log("Song Title: " + data.tracks.items[0].name)
-        console.log("Album: " + data.tracks.items[0].album.name)
-        console.log("Preview the song: " + data.tracks.items[0].preview_url)
+        var results = ("\nArtist: " + data.tracks.items[0].artists[0].name  + "\nSong Title: " + data.tracks.items[0].name  + "\nAlbum: " + data.tracks.items[0].album.name  + "\nPreview the song: " + data.tracks.items[0].preview_url);
+        console.log(results)
+        log(results)
     });
 }
 var request = require("request");
 function movie(input) {
-    
+
     request("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
 
 
         if (!error && response.statusCode === 200) {
-
-            console.log("Title: " + JSON.parse(body).Title)
-            console.log("Release Year: " + JSON.parse(body).Year)
-            console.log("IMDB Rating: " + JSON.parse(body).imdbRating)
-            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value)
-            console.log("Countries of Production: " + JSON.parse(body).Country)
-            console.log("Language: " + JSON.parse(body).Language)
-            console.log("Actors: " + JSON.parse(body).Actors)
-            console.log("Plot: " + JSON.parse(body).Plot)
+            var data = JSON.parse(body)
+            var results = ("\nTitle: " + data.Title + "\nRelease Year: " + data.Year + "\nIMDB Rating: " + data.imdbRating + "\nRotten Tomatoes Rating: " + data.Ratings[1].Value + "\nCountries of Production: " + data.Country + "\nLanguage: " + data.Language + "\nActors: " + data.Actors + "\nPlot: " + data.Plot)
+            console.log(results)
+            log(results)
         }
     })
 }
 
-function concert(input){
+function concert(input) {
     var request = require("request");
     request("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp", function (error, response, body) {
         if (!error && response.statusCode === 200) {
-        // console.log(input)
-        // var data = JSON.parse(body)
-        console.log(body)
+            // console.log(input)
+            // var data = JSON.parse(body)
+            console.log(body)
         }
     })
-    
+}
+function log(results) {
+    fs.appendFile("log.txt", results, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    })
 
 }
-function doIt(){
-    console.log("doing it")
+
+var fs = require("fs")
+function doIt() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if(err){
+            console.log(err)
+        }
+        var output = data.split(",")
+        song(output[1])
+    })
 }
 var command = process.argv[2]
 var term = ""
@@ -77,17 +85,17 @@ else if (command === "movie-this") {
     }
 }
 else if (command === "concert-this") {
-        if (process.argv.length == 3) {
-            console.log("Please enter an artist or band name.")
+    if (process.argv.length == 3) {
+        console.log("Please enter an artist or band name.")
+    }
+    else {
+        for (i = 3; i < process.argv.length; i++) {
+            term = term + process.argv[i] + " "
         }
-        else {
-            for (i = 3; i < process.argv.length; i++) {
-                term = term + process.argv[i] + " "
-            }
-            concert(term)
-        }
+        concert(term)
+    }
 }
-else if ( command === "do-what-it-says"){
+else if (command === "do-what-it-says") {
     doIt()
 }
 
